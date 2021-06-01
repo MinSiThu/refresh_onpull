5963c00159adbceac28bac202d825ee5b401e3c5
Min Si Thu <archimedes557@gmail.com>
2021-06-01 09:19:47 -0700

    feat: Refresh on Pull

---
+++ android/app/src/main/kotlin/com/example/refresh_onpull/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.refresh_onpull
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'dart:math';
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+main(List<String> args) {
+  return runApp(RefreshOnPull());
+}
+
+class RefreshOnPull extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Refresh On Pull",
+        debugShowCheckedModeBanner: false,
+        home: RefreshOnPullPage());
+  }
+}
+
+class RefreshOnPullPage extends StatefulWidget {
+  _RefreshOnPullPageState createState() => _RefreshOnPullPageState();
+}
+
+class _RefreshOnPullPageState extends State<RefreshOnPullPage> {
+  List _list;
+  var random;
+
+  var refreshKey = GlobalKey<RefreshIndicatorState>();
+
+  @override
+  void initState() {
+    super.initState();
+    random = Random();
+    refreshList();
+  }
+
+  Future<Null> refreshList() async {
+    refreshKey.currentState?.show(atTop: false);
+    await Future.delayed(Duration(seconds: 2));
+    setState(() {
+      _list = List.generate(random.nextInt(20), (i) => "Item $i");
+    });
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Refresh On Pull"),
+          backgroundColor: Colors.deepPurpleAccent,
+        ),
+        body: RefreshIndicator(
+          key: refreshKey,
+          onRefresh: refreshList,
+          child: ListView.builder(
+              itemBuilder: (context, index) =>
+                  ListTile(title: Text(_list[index]))),
+        ));
+  }
+}
